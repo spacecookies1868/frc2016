4dfa28b43c41b9049589dba5670d864ad557bd68
nandeeka <nandeeka@389cb0df-f4b6-43fc-ae77-39cd09cc6615>
2016-01-31 16:32:18 +0000

    Changed the joystick and motor ports to accurately reflect the current configuration on the driver station and kit of parts robot. -Nandeeka
    git-svn-id: https://spacecookies.svn.beanstalkapp.com/spacecookies/frc2016@726 389cb0df-f4b6-43fc-ae77-39cd09cc6615

--- robot/trunk/src/RobotPorts2016.h
+++ robot/trunk/src/RobotPorts2016.h
@@ -6,7 +6,7 @@
 static const int LEFT_DRIVE_MOTOR_A_PWM_PORT 			= 0;
 static const int LEFT_DRIVE_MOTOR_B_PWM_PORT			= 1;
 static const int RIGHT_DRIVE_MOTOR_A_PWM_PORT			= 2;
-static const int RIGHT_DRIVE_MOTOR_B_PWM_PORT			= 3;
+static const int RIGHT_DRIVE_MOTOR_B_PWM_PORT			= 5;
 
 // ***************** DIGITAL I/O PORTS *****************
 
@@ -31,8 +31,8 @@ static const int PNEUMATICS_CONTROL_MODULE_ID			= 0;
 static const int GEAR_SHIFT_SOLENOID_PORT				= 7;
 
 // ***************** JOYSTICK USB PORTS *****************
-static const int LEFT_JOY_USB_PORT						= 1;
-static const int RIGHT_JOY_USB_PORT						= 0;
+static const int LEFT_JOY_USB_PORT						= 0;
+static const int RIGHT_JOY_USB_PORT						= 1;
 static const int OPERATOR_JOY_USB_PORT					= 2;
 
 // ***************** BUTTON PORTS *****************
