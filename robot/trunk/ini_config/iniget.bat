ff48e60b8fb0343efc186a149995b12f1b11c72b
Katy_Woo <Katy_Woo@389cb0df-f4b6-43fc-ae77-39cd09cc6615>
2016-02-17 03:34:52 +0000

    Framework that was deployed on robot :) Also, cleaned ini file so it's basic version.
    Katy,Lynn, Divya
    
    git-svn-id: https://spacecookies.svn.beanstalkapp.com/spacecookies/frc2016@745 389cb0df-f4b6-43fc-ae77-39cd09cc6615

--- robot/trunk/ini_config/iniget.bat
+++ robot/trunk/ini_config/iniget.bat
@@ -1,2 +1,2 @@
 cd "%~d0%~p0"
-psftp -b ftpgetcmds.txt lvuser@roboRIO-1868.local
+psftp -b ftpgetcmds.txt lvuser@roboRIO-1868-frc.local
--- robot/trunk/ini_config/iniput.bat
+++ robot/trunk/ini_config/iniput.bat
@@ -1,2 +1,2 @@
 cd "%~d0%~p0"
-psftp -b ftpputcmds.txt lvuser@roboRIO-1868.local
+psftp -b ftpputcmds.txt lvuser@roboRIO-1868-frc.local
